b3add658b6068171fcbfa5fd3fbda1b929ac760a
Jacob Lang <130790079+JL1172@users.noreply.github.com>
2024-04-26 18:26:26 -0400

    ran all tests and verified that all tests pass and there is no errors or breakages.

--- prisma/delete.ts
+++ prisma/delete.ts
@@ -4,10 +4,12 @@ const prisma = new PrismaClient();
 export async function resetDb() {
   await prisma.verificationCode.deleteMany();
   await prisma.jwtToken.deleteMany();
+  await prisma.customer.deleteMany();
   await prisma.user.deleteMany();
   await prisma.$executeRaw`ALTER SEQUENCE "VerificationCode_id_seq" RESTART WITH 1`;
   await prisma.$executeRaw`ALTER SEQUENCE "User_id_seq" RESTART WITH 1`;
   await prisma.$executeRaw`ALTER SEQUENCE "JwtToken_id_seq" RESTART WITH 1`;
+  await prisma.$executeRaw`ALTER SEQUENCE "Customer_id_seq" RESTART WITH 1`;
   prisma.$disconnect();
 }
 resetDb();
